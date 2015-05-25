b934618b329495f424bdd1277df65898c5ea118f
blakealtman123 <blakealtman123@gmail.com>
2015-05-25 23:13:40 +0200

    answers

---
+++ exercises/d6/6a.rb
@@ -0,0 +1,16 @@
+E1
+1. git init command initializes a repo.
+2. git add adds stuff to the stage, git commit adds stuff to the local repository from stage and wipes stage
+3. Stage is just stored in the chage, committed versions are in the local repository. lets you split work into seperate commits and prevents breaking the code.
+4. git add . 
+git commit -m "message here"
+5. git reset <file>
+6. So that you remember what you did in each version and other people can follow along.
+7. git checkout
+8. exit
+
+E2
+1. Github is a company that manages a set of remote repositories, git manages the otehr repositories on your computer. Git exists without Github but not the other way around.
+2. It makes it easier to use git.
+3. putting/updating stuff online.
+4. taking the stuff online and updating it on your computer.
---
+++ exercises/d6/6b.rb
@@ -0,0 +1,13 @@
++E1
++1. When the client requests something, it is asking for some file
++2. client
++3. to servers, based on the ip address of the servers
++4. parameters sent to applications, spits back out the html 
++5. response, back to client, html/css
++6. The client is able to interact with it.
++
++E2
++1. 404 Error page
++2. Unable to process page for whatever reason
++3. Page doesnt exist
++4. yes
